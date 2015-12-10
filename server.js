2302f9a2cc8b5f4af4cb5c0fade0e42bf897c019
Owais <soac@outlook.com>
2015-12-10 22:42:56 +0500

    Added root path

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ var NeonURL = 'http://nu.edu.pk/NeONStudent/';
 // Remember Cookies
 //var request = request.defaults({ jar: true }) // Saving it in session
 
+app.get('/', function (req, res) {
+    res.send("Start from GET /load");
+}
+
 app.get('/load', function (req, res) {
 	// Create Cookie jar	
 	req.session.cookies = request.jar()
