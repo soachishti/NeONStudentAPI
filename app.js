b02c192a399211eb46b31a6b5eabb24f3d84d2c0
Owais <soac@outlook.com>
2016-01-06 18:09:37 +0500

    Module fix

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 
 var app 	= require('./server.js');
 var request = require('request');
-var async = require('async');
 var cheerio = require('cheerio');
-var wait = require('wait.for');
 global.db 			= require("./db.js");
 global.setting  	= require("./setting.js");
 global.misc 		= require("./misc.js");
