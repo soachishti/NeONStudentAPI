cf45ba93315553bbde7986a6521892a754002e32
Owais <soac@outlook.com>
2016-01-13 20:14:57 +0500

    Minor

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ var uuid 		= require('node-uuid');
 var crypto 		= require('crypto');
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-	host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
-	user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
-	password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
-	port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
-	database : process.env.OPENSHIFT_APP_NAME
+	host     : process.env.OPENSHIFT_MYSQL_DB_HOST || '127.0.0.1',
+	user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root',
+	password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD || '',
+	port     : process.env.OPENSHIFT_MYSQL_DB_PORT || 3306,
+	database : process.env.OPENSHIFT_APP_NAME || 'neonapi'
 }); 
 
 connection.connect(function(err) {
@@ -20,7 +20,7 @@ connection.connect(function(err) {
 
 var dbConfig 	= {
 	algorithm 	: 'aes-256-ctr',
-	password  	: process.env.NEON_SECURE_PASSWORD	
+	password  	: process.env.NEON_SECURE_PASSWORD || 'ha6hG123Jhajy'
 };
   
 function encrypt(text){
@@ -54,11 +54,12 @@ connection.query("DELETE FROM UserData WHERE expire < ?" , [Math.round(new Date(
 module.exports = {
 	CreateUser: function() {
 		var id = uuid.v1();
-		console.log("Creating User " + id);		
+		console.log("Creating User " + id);	
+	
 		var data  = {
 			key		: id,
 			value	: encrypt('{}'),
-			expire	: Math.round(new Date().getTime() / 1000)
+			expire	: new Date()
 		};
 		
 		var query = connection.query('INSERT INTO UserData SET ?', data, function(err, result) {});
@@ -66,7 +67,7 @@ module.exports = {
 	},
 	DeleteUser: function (key, value) {
 		console.log("Delete User " + key);
-		var sql = "DELETE FROM UserData WHERE key = " + connection.escape(key);
+		var sql = "DELETE FROM UserData WHERE `key` = " + connection.escape(key);
 		connection.query(sql, function (err, result) {
 			if (err) throw err;
 			console.log('Delete Expired data: Count ' + result.affectedRows + ' rows');
@@ -74,30 +75,28 @@ module.exports = {
 	},
 	UpdateUser: function (key, value) {
 		console.log("Update User")
-				
-		var sql = "UPDATE UserData SET value = "+ connection.escape(encrypt(JSON.stringify(value))) +
-				", expire = " + connection.escape((Math.round(new Date().getTime() / 1000) + global.setting.DataStoreTimeout))  + 
-				" WHERE key = " + connection.escape(key) + ";";
-		console.log(sql);
+		var expireTime = Math.round(new Date().getTime() / 1000) + global.setting.DataStoreTimeout;
+		
+		var sql = "UPDATE UserData SET `value` = "+ connection.escape(encrypt(JSON.stringify(value))) +
+				", `expire` = " + connection.escape(new Date(expireTime * 1000))  + 
+				" WHERE `key` = " + connection.escape(key) + ";";
 	
 		connection.query(sql, function (err, result) {
 			if (err) throw err;
-			console.log('changed ' + result.changedRows + ' rows');
+			console.log('changed ' + result.affectedRows + ' rows');
 		});
 	},
 	GetUser: function (key, callback) {
 		// Update expire date
-		
-		var sql = "UPDATE UserData SET expire = " + connection.escape((Math.round(new Date().getTime() / 1000) + global.setting.DataStoreTimeout)) + 
-			" WHERE key = " + connection.escape(key)  + ";";
+		var expireTime = Math.round(new Date().getTime() / 1000) + global.setting.DataStoreTimeout;
+
+		var sql = "UPDATE UserData SET `expire` = " + connection.escape(new Date(expireTime * 1000)) + 
+			" WHERE `key` = " + connection.escape(key)  + ";";
 		console.log(sql);
 		
-		connection.query(sql, function (err, result) {
-			if (err) throw err;
-			console.log('changed ' + result.changedRows + ' rows');
-		});
+		connection.query(sql, function (err, result) {});
 		
-		var sql    = "SELECT value FROM UserData WHERE key = " +  connection.escape(key);
+		var sql    = "SELECT value FROM UserData WHERE `key` = " +  connection.escape(key) + ";";
 		connection.query(sql, function(err, row) {
 			if (typeof row != 'undefined') {
 				return callback(JSON.parse(decrypt(row.value)));
