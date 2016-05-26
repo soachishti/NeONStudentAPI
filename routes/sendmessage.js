aea4656864d400adaf45a885f57d669b7dacd4a9
Owais <soachishti@outlook.com>
2016-05-27 01:54:47 +0500

    Send message using mailgun added

--- routes/sendmessage.js
+++ routes/sendmessage.js
@@ -1,41 +1,47 @@
 var nodemailer = require('nodemailer');
+var sendmailTransport = require('nodemailer-sendmail-transport');
 
 module.exports = function (app, request) {		
 	/**
-	 * @api {get} /transcript Transcript
-	 * @apiName Student Transcript
+	 * @api {post} /sendmessage Send feedback message to author.
+	 * @apiName SendMessage
 	 * @apiGroup Info
 	 *
-	 * @apiSuccess {String} result JSON formated array data with index semester number and data inside each.
-	 * @apiError error Reason for failing to get data.
+	 * @apiSuccess {String} true
+	 * @apiError {String} false
 	 */
-	app.get('/sendmessage', function(req, res) {
-
-        // create reusable transporter object using the default SMTP transport
-        var transporter = nodemailer.createTransport("sendmail");
-        
-        // setup e-mail data with unicode symbols
-        var mailOptions = {
-            from: req.param.name + " <" + req.param.email + ">", // sender address
-            to: 'p146011@nu.edu.pk', // list of receivers
-            subject: 'NeON Student Mobile | Contact', // Subject line
-            text: req.param.message, // plaintext body
-        };
-
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, function(error, info){
-            if(error){
-                res.send({
-                    error: "false"
-                });
-                console.log(error);
-            }
-            else {
-                console.log('Message sent: ' + info.response);
-                res.send({
-                    success: "true"
-                });
-            }
-        });
+	app.post('/sendmessage', function(req, res) {    
+        var api_key = 'key-5929d54ff5836aa34a59e2764c29225a';
+        var domain = 'sandboxe2e07f9c730543a8bed61289e68a0a2d.mailgun.org';
+        var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+        console.log(req);
+        if (!req.body.name || !req.body.email || !req.body.text) {
+            res.send({
+                error: "Invalid Name, Email, Message"
+            });
+        }
+        else {   
+            var data = {
+                from: req.body.name + " <" + req.body.email + ">", // sender address
+                to: 'p146011@nu.edu.pk', // list of receivers
+                subject: 'NeON Student Mobile | Contact', // Subject line
+                text: req.body.text, // plaintext body
+            };
+            mailgun.messages().send(data, function (error, body) {
+                if(error){
+                    res.statusCode = 406;
+                    res.send({
+                        error: "false"
+                    });
+                    console.log(error);
+                }
+                else {
+                    //console.log('Message sent: ' + body);
+                    res.send({
+                        success: "true"
+                    });
+                }
+            });
+        }
     })
 };
